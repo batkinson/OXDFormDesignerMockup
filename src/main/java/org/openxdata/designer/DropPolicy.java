128600c9187dc5dbb39252c62d2d5ecaf798c53d
Brent Atkinson <brent.atkinson@gmail.com>
2011-04-13 22:57:33 +0300

    Fixed drag and drop so it only allows valid drops.

---
+++ src/main/java/org/openxdata/designer/DropPolicy.java
@@ -0,0 +1,35 @@
+package org.openxdata.designer;
+
+import org.apache.pivot.collections.List;
+import org.apache.pivot.collections.Sequence;
+import org.openxdata.designer.util.Form;
+import org.openxdata.designer.util.Page;
+import org.openxdata.designer.util.Question;
+
+public class DropPolicy {
+
+	public static boolean allowDrop(Object source, Object target) {
+
+		if (source == null || target == null)
+			return false;
+
+		if (source instanceof Question)
+			return target instanceof Page || target instanceof Question;
+
+		if (source instanceof Page)
+			return target instanceof Form;
+
+		return false;
+	}
+
+	public static boolean allowDrop(List<Object> treeData,
+			Sequence.Tree.Path sourcePath, Sequence.Tree.Path targetPath) {
+
+		if (sourcePath == null || targetPath == null)
+			return false;
+
+		Object sourceObject = Sequence.Tree.get(treeData, sourcePath);
+		Object targetObject = Sequence.Tree.get(treeData, targetPath);
+		return allowDrop(sourceObject, targetObject);
+	}
+}
--- src/main/resources/org/openxdata/designer/dragdrop.js
+++ src/main/resources/org/openxdata/designer/dragdrop.js
@@ -1,9 +1,11 @@
 importPackage(java.lang);
 importPackage(org.apache.pivot.wtk);
+importPackage(org.openxdata.designer);
+importPackage(org.openxdata.designer.util);
 
 var formTreeDropTarget = new DropTarget() {
 
-	dragEnter: 
+    dragEnter: 
     function(component, dragContent, supportedDropActions, userDropAction) {
         var dropAction = null;
         if (dragContent.containsFileList()
@@ -11,83 +13,105 @@ var formTreeDropTarget = new DropTarget() {
             dropAction = DropAction.COPY;
         }
         else if (dragContent.containsValue('node')) {
-        	dropAction = DropAction.MOVE;
+            dropAction = DropAction.MOVE;
         }
         return dropAction;
     },
 
     dragExit:
-    function(component) {
-    },
+    function(component) { },
 
     dragMove: 
     function(component, dragContent, supportedDropActions, x, y, userDropAction) {
-    	if (dragContent.containsValue('node') && designTree.getNodeAt(y) != null) {
-    		return DropAction.MOVE;
-    	}
-    	else if (dragContent.containsFileList())
-    		return DropAction.COPY;
-    	else
-    		return null;
+    
+        if (dragContent.containsValue('node')) {
+            var treeData = designTree.getTreeData();
+            var sourcePath = dragContent.getValue('path');
+            var targetPath = designTree.getNodeAt(y);
+            if (DropPolicy.allowDrop(treeData, sourcePath, targetPath))
+                return DropAction.MOVE;
+            }
+        else if (dragContent.containsFileList())
+            return DropAction.COPY;
+    
+        return null;
     },
 
     userDropActionChange: 
     function(component, dragContent, supportedDropActions, x, y, userDropAction) {
-    	if (dragContent.containsValue('node') && designTree.getNodeAt(y) != null)
-    		return DropAction.MOVE;
-    	else if (dragContent.containsFileList())
-    		return DropAction.COPY;
-    	else
-    		return null;
+        if (dragContent.containsValue('node')) {
+            var treeData = designTree.getTreeData();
+            var sourcePath = dragContent.getValue('path');
+            var targetPath = designTree.getNodeAt(y);
+            if (DropPolicy.allowDrop(treeData, sourcePath, targetPath))
+                return DropAction.MOVE;
+        }
+        else if (dragContent.containsFileList())
+            return DropAction.COPY;
+
+        return null;
     },
 
     drop: 
     function(component, dragContent, supportedDropActions, x, y, userDropAction) {
-    	// Oddly, in this method x,y are display relative
-    	var display = component.getDisplay();
-    	var dropLocation = component.mapPointFromAncestor(display, x, y);
+        // Oddly, in this method x,y are display relative
+        var display = component.getDisplay();
+        var dropLocation = component.mapPointFromAncestor(display, x, y);
         var dropAction = null;
         if (dragContent.containsFileList()) {
             return application.drop(dragContent);
         }
-        else if (dragContent.containsValue('node') && designTree.getNodeAt(dropLocation.y) != null) {
-        	dragContent.putValue('targetPath', designTree.getNodeAt(dropLocation.y));
-        	return application.drop(dragContent);
+        else if (dragContent.containsValue('node')) {
+            var treeData = designTree.getTreeData();
+            var sourcePath = dragContent.getValue('path');
+            var targetPath = designTree.getNodeAt(dropLocation.y);
+            if (DropPolicy.allowDrop(treeData, sourcePath, targetPath)){
+                dragContent.putValue('targetPath', targetPath);
+                return application.drop(dragContent);
+            }
         }
+
         this.dragExit(component);
         return dropAction;
     }
 };
 
 var designTreeDragSource = new DragSource() {
-	
-    beginDrag: function(component, x, y) {
-    	return true;
+
+    beginDrag:
+    function(component, x, y) {
+        var obj = designTree.getSelectedNode();
+        return obj instanceof Page || obj instanceof Question;
     },
- 
-    endDrag: function(component, dropAction) {
-    },
- 
-    getContent: function() {
+
+    endDrag:
+    function(component, dropAction) { },
+
+    getContent: 
+    function() {
         var content = new LocalManifest();
         content.putValue('node',designTree.getSelectedNode());
         content.putValue('path',designTree.getSelectedPath());
         return content;
     },
- 
-    getOffset: function() {
+
+    getOffset:
+    function() {
+        return null; // Not used for native drags
+    },
+
+    getRepresentation: 
+    function() {
         return null; // Not used for native drags
     },
  
-    getRepresentation: function() {
-        return null; // Not used for native drags
-    },
- 
-    getSupportedDropActions: function() {
+    getSupportedDropActions:
+    function() {
         return DropAction.MOVE.getMask();
     },
  
-    isNative: function() {
+    isNative: 
+    function() {
         return false;
     }
 }
