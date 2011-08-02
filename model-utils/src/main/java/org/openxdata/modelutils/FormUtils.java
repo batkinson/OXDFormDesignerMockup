21ec1fb625495157f3374d8156e48709a17aeb76
Brent Atkinson <brent.atkinson@gmail.com>
2011-08-02 01:49:25 -0400

    Added utils method to fix forms after binding changes.

---
+++ model-utils/src/main/java/org/openxdata/modelutils/FormUtils.java
@@ -0,0 +1,102 @@
+package org.openxdata.modelutils;
+
+import java.util.Vector;
+
+import org.fcitmuk.epihandy.Condition;
+import org.fcitmuk.epihandy.FormDef;
+import org.fcitmuk.epihandy.QuestionDef;
+import org.fcitmuk.epihandy.SkipRule;
+import org.fcitmuk.epihandy.ValidationRule;
+
+public class FormUtils {
+
+	/**
+	 * Useful to clean up references to an old form variable name after it is
+	 * updated. It *does not* update the value in the form itself. This is
+	 * provided as a utility, but the data model should guarantee this when
+	 * setting the method. If anything, this is precise documentation of the
+	 * necessary updates that it should include.
+	 * 
+	 * @param form
+	 *            the form to update.
+	 * @param newName
+	 *            the new bind variable name for the form.
+	 */
+	@SuppressWarnings("unchecked")
+	public static void updateFormVarName(FormDef form, String oldName,
+			String newName) {
+
+		if (newName == null)
+			throw new IllegalArgumentException(
+					"form requires a non-null variable name");
+
+		// Use varName/ in attempt to avoid replacing non-variable refs
+		String varRegex = String.format("/?%s/", oldName);
+		String varReplacement = String.format("/%s/", newName);
+
+		// Update the description template
+		String dt = form.getDescriptionTemplate();
+		if (dt != null)
+			form.setDescriptionTemplate(dt.replaceAll(varRegex, varReplacement));
+
+		// Update questions
+		QuestionTree qTree = QuestionTree.constructTreeFromFormDef(form);
+		updateFormVarName(qTree, varRegex, varReplacement);
+
+		// Update the validation rule condition operands
+		for (ValidationRule vr : (Vector<ValidationRule>) form
+				.getValidationRules()) {
+			for (Condition vc : (Vector<Condition>) vr.getConditions()) {
+				String value = vc.getValue();
+				if (value != null) {
+					vc.setValue(value.replaceFirst(varRegex, varReplacement));
+				}
+				String secondValue = vc.getSecondValue();
+				if (secondValue != null) {
+					vc.setSecondValue(secondValue.replaceFirst(varRegex,
+							varReplacement));
+				}
+			}
+		}
+
+		// Update the skip rule condition operands
+		for (SkipRule sr : (Vector<SkipRule>) form.getSkipRules()) {
+			for (Condition sc : (Vector<Condition>) sr.getConditions()) {
+				String value = sc.getValue();
+				if (value != null) {
+					sc.setValue(value.replaceFirst(varRegex, varReplacement));
+				}
+				String secondValue = sc.getSecondValue();
+				if (secondValue != null) {
+					sc.setSecondValue(secondValue.replaceFirst(varRegex,
+							varReplacement));
+				}
+			}
+		}
+	}
+
+	private static void updateFormVarName(QuestionTree tree,
+			String matchPattern, String replacement) {
+
+		// If the question has child questions, update them
+		if (!tree.isLeaf()) {
+			for (QuestionTree childTree : tree.getChildren())
+				updateFormVarName(childTree, matchPattern, replacement);
+		}
+
+		// Root doesn't contain a question, no-op
+		if (tree.isRoot())
+			return;
+
+		// Update the question...
+		QuestionDef q = tree.getQuestion();
+
+		String text = q.getText();
+		if (text != null)
+			q.setText(text.replaceAll(matchPattern, replacement));
+
+		String varName = q.getVariableName();
+		if (varName != null)
+			q.setVariableName(varName.replaceFirst(matchPattern, replacement));
+	}
+}
---
+++ model-utils/src/test/java/org/openxdata/modelutils/FormUtilsTest.java
@@ -0,0 +1,78 @@
+package org.openxdata.modelutils;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+
+import org.apache.commons.io.IOUtils;
+import org.fcitmuk.epihandy.FormDef;
+import org.fcitmuk.epihandy.xform.EpihandyXform;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FormUtilsTest {
+
+	private static Logger log = LoggerFactory.getLogger(FormUtilsTest.class);
+
+	static String sampleXml;
+	static InputStream sampleStream;
+
+	FormDef sampleForm;
+
+	@BeforeClass
+	public static void oneTimeSetup() throws IOException {
+
+		log.debug("reading sample xform");
+		InputStream sampleStream = FormUtilsTest.class
+				.getResourceAsStream("/sample.xml");
+		try {
+			sampleXml = IOUtils.toString(sampleStream);
+			log.debug("read sample xform:\n" + sampleXml);
+		} catch (IOException e) {
+			String msg = "Failed to read xform";
+			log.error(msg, e);
+			throw e;
+		}
+
+		// So we can reset
+		FormUtilsTest.sampleStream = new ByteArrayInputStream(
+				sampleXml.getBytes());
+	}
+
+	@Before
+	public void setUp() {
+		sampleForm = EpihandyXform
+				.fromXform2FormDef(new StringReader(sampleXml));
+	}
+
+	@After
+	public void tearDown() throws IOException {
+		FormUtilsTest.sampleStream.reset();
+	}
+
+	@Test
+	public void testUpdateFormVarName() {
+
+		log.debug("testing variable name updates");
+
+		String newName = "newname";
+		String oldName = sampleForm.getVariableName();
+
+		sampleForm.setVariableName(newName);
+		FormUtils.updateFormVarName(sampleForm, oldName, newName);
+
+		String result = ModelToXML.convert(sampleForm);
+
+		log.debug("converted xml after varname update:\n" + result);
+
+		assertTrue("form should not contain old variable name: " + oldName,
+				result.indexOf(oldName) == -1);
+	}
+}
